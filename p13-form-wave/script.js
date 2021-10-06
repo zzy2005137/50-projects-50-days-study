90edbfeeafa677b0c0b1da03a44731f369f8c216
zzy <765205884@qq.com>
2021-10-06 22:42:30 +0800

    form wave

---
+++ p13-form-wave/script.js
@@ -0,0 +1,13 @@
+const labels = document.querySelectorAll(".form-control label")
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style ="transition-delay:${idx * 20}ms">${letter}</span>`
+    )
+    .join("")
+})
+
+console.log(labels[0].innerHTML)
